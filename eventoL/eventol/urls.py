0acea87fa20be0745e852823c8d69181492192b8
FedeG <federicog@exo.com.ar>
2016-03-07 00:52:58 -0300

    Archivos de vista y urls para index.html + static

--- eventoL/eventol/urls.py
+++ eventoL/eventol/urls.py
@@ -17,8 +17,11 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^api/', include('api.urls')),
     url(r'^admin/', admin.site.urls),
-]
+    url(r'^/', include('event.urls'))
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
